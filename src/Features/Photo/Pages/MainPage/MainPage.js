83170ffe0917059d33febdcb39c6bd4637019cf8
ngochoanit <phamngochoan.it@gmail.com>
2021-08-18 14:33:35 +0700

    clean  react artchitecture

---
+++ src/Features/Photo/Pages/MainPage/MainPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+MainPage.propTypes = {
+
+}
+
+function MainPage(props) {
+    return (
+        <div>
+            <h1>Photo Main Page</h1>
+        </div>
+    )
+}
+
+export default MainPage
